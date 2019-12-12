7ca0e0437317ee504bea6929978d5854a9da5c08
JHay0112 <jordanhay0112@gmail.com>
2019-12-13 11:17:49 +1300

    Slogan

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@
 function exitLoadingScreen() {
     loadingScreen = document.getElementById("loading-screen"); // Get loading screen
     loadingSpinner = document.getElementById("loading-spinner"); // Get loading spinner
-    backgroundImage = document.getElementById("background-image");
+    backgroundImage = document.getElementById("background-image"); // Get background image
 
     loadingScreen.style.animation = "fade-out 2s 1s forwards ease"; // Fade out
     loadingSpinner.style.animation = "spin 1s infinite linear, fade-out 2s forwards"; // Fade out while still spinning
-    backgroundImage.style.animation = "background-zoom 1.5s 1.25s forwards";
+    backgroundImage.style.animation = "background-zoom 1.5s 1.25s forwards"; // Gentle zoom thing
 }
