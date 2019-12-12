5b11e9c2aca288052d6cbef117a8678eface9306
JHay0112 <jordanhay0112@gmail.com>
2019-12-12 19:42:37 +1300

    Basic loading screen

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*
+    js/main.js
+    Author: Jordan Hay
+*/
+
+// Exit loading screen once page has loaded
+function exitLoadingScreen() {
+    loadingScreen = document.getElementById("loading-screen"); // Get loading screen
+    loadingSpinner = document.getElementById("loading-spinner"); // Get loading spinner
+
+    loadingScreen.style.animation += "fade-out 2s 1s forwards"; // Fade out
+    loadingSpinner.style.animation += "spin 1s infinite linear, fade-out 2s forwards"; // Fade out while still spinning
+}
