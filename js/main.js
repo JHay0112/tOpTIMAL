127194722bf8a3c0ba4a953f4fa7ba15f228a2e4
JHay0112 <jordanhay0112@gmail.com>
2019-12-14 16:19:20 +1300

    Slideshow

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@
     Author: Jordan Hay
 */
 
+// Sleep function
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 // Exit loading screen once page has loaded
 function exitLoadingScreen() {
     loadingScreen = document.getElementById("loading-screen"); // Get loading screen
@@ -12,4 +17,31 @@ function exitLoadingScreen() {
     loadingScreen.style.animation = "fade-out 2s 1s forwards ease"; // Fade out
     loadingSpinner.style.animation = "spin 1s infinite linear, fade-out 2s forwards"; // Fade out while still spinning
     backgroundImage.style.animation = "background-zoom 1.5s 1.25s forwards"; // Gentle zoom thing
+}
+
+// Slideshow
+async function runSlideShow() {
+    images = [
+        "holly.jpg",
+        "elaine.jpg",
+        "heaphy.jpg",
+        "phaedra.jpg"
+    ];
+
+    sideImg = document.getElementById("side-img");
+
+    imageURL = "url('img/";
+    imageURL = imageURL.concat(images[0], "')");
+
+    for(var i = 0; i <= images.length; i++) {
+        if(i >= images.length) {
+            i = 0;
+        }
+        sideImg.style.animation = "slideshow-slide-out 0.3s forwards";
+        await sleep(300);
+        sideImg.style.backgroundImage = "url(\"img/".concat(images[i], "\")");
+        sideImg.style.animation = "slideshow-slide-in 0.3s forwards";
+        await sleep(5000);
+    }
+
 }
