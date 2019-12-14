8e1ca12b8623c25a3554a65616a616e73feb1b64
JHay0112 <jordanhay0112@gmail.com>
2019-12-14 16:28:02 +1300

    Position and timing changes

--- js/main.js
+++ js/main.js
@@ -37,10 +37,10 @@ async function runSlideShow() {
         if(i >= images.length) {
             i = 0;
         }
-        sideImg.style.animation = "slideshow-slide-out 2s forwards";
+        sideImg.style.animation = "slideshow-slide-out 1s forwards";
         await sleep(2000);
         sideImg.style.backgroundImage = "url(\"img/".concat(images[i], "\")");
-        sideImg.style.animation = "slideshow-slide-in 2s forwards";
+        sideImg.style.animation = "slideshow-slide-in 1s forwards";
         await sleep(8000);
     }
 
