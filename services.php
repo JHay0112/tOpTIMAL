6ad2baa7d1ff1fa512842755b020307eac3b212a
JHay0112 <jordanhay0112@gmail.com>
2020-01-07 10:44:59 +1300

    Online training plus simplification

--- services.php
+++ services.php
@@ -13,40 +13,38 @@
 
 <main class="row">
     <aside class="col-5" id="side-img" style="background-image: url('img/abel_tasman.jpg')"></aside>
-    <div class="row">
-        <div class="col-5"></div>
-        <section id="subpage-content" class="col-7">
-            <h1 class="desktop-only">What we can offer</h1>
-            <h1 class="mobile-only">What we can offer</h1>
-            <hr />
+    <div class="col-5"></div>
+    <section id="subpage-content" class="col-7">
+        <h1 class="desktop-only">What we can offer</h1>
+        <h1 class="mobile-only">What we can offer</h1>
+        <hr />
 
-            <h2>Optimal Coaching</h2>
-            <h5>Duration varies • Price varies</h5>
-            <p>Wellness, fitness & lifestyle mentoring that is tailored to suit you. This is life coaching in its exponential form. Guaranteed results, plus optimal growth for personal wellbeing. You will change your perceived limits.</p>
+        <h2>Optimal Coaching</h2>
+        <h5>Duration varies • Price varies</h5>
+        <p>Wellness, fitness & lifestyle mentoring that is tailored to suit you. This is life coaching in its exponential form. Guaranteed results, plus optimal growth for personal wellbeing. You will change your perceived limits.</p>
 
-            <h2>Create and Ideate Workshops</h2>
-            <h5>Duration varies • Price varies</h5>
-            <p>A variety of different ideation opportunities with supportive and motivational mentorship.</p>
-            <p>These prove to be a great challenge and team building opportunity for organisations who wish to improve collegiality and growth mindsets.</p>
+        <h2>Create and Ideate Workshops</h2>
+        <h5>Duration varies • Price varies</h5>
+        <p>A variety of different ideation opportunities with supportive and motivational mentorship.</p>
+        <p>These prove to be a great challenge and team building opportunity for organisations who wish to improve collegiality and growth mindsets.</p>
 
-            <h2>Challenge workshops</h2>
-            <h5>Duration varies • Price varies</h5>
-            <p>Aimed at those who wish to mentally and or physically push themselves to achieve new goals. These allow participants to strive for personal greatness. Tutoring, mentorship and support.</p>
+        <h2>Challenge workshops</h2>
+        <h5>Duration varies • Price varies</h5>
+        <p>Aimed at those who wish to mentally and or physically push themselves to achieve new goals. These allow participants to strive for personal greatness. Tutoring, mentorship and support.</p>
 
-            <h2>NCEA Tutoring</h2>
-            <h5>Duration varies • Discounts available for 5 + sessions</h5>
-            <p>Tutoring in a range of subject areas, especially English, Media and NCEA literacy.</p>
-            <p>Also available are study and self management workshops for students and teachers.</p>
+        <h2>NCEA Tutoring</h2>
+        <h5>Duration varies • Discounts available for 5 + sessions</h5>
+        <p>Tutoring in a range of subject areas, especially English, Media and NCEA literacy.</p>
+        <p>Also available are study and self management workshops for students and teachers.</p>
 
-            <h2>Edu coaching / Wellness workshops</h2>
-            <h5>Duration varies • Price varies</h5>
-            <p>Workshops, professional learning opportunities and optimal coaching specifically aimed at Educators who wish to improve their lifestyle or for education workplaces that want to increase staff wellness and participation.</p>
+        <h2>Edu coaching / Wellness workshops</h2>
+        <h5>Duration varies • Price varies</h5>
+        <p>Workshops, professional learning opportunities and optimal coaching specifically aimed at Educators who wish to improve their lifestyle or for education workplaces that want to increase staff wellness and participation.</p>
 
-            <h2>KetOPTIMAL Coaching</h2>
-            <h5>Duration varies • Price varies</h5>
-            <p>KETOGENIC lifestyle training and coaching, mentoring and motivation. Ditching carbs ain’t hard! </p>
-        </section>
-    </div>
+        <h2>KetOPTIMAL Coaching</h2>
+        <h5>Duration varies • Price varies</h5>
+        <p>KETOGENIC lifestyle training and coaching, mentoring and motivation. Ditching carbs ain’t hard! </p>
+    </section>
 </main>
 
 
