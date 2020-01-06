b59a6b5ae908c90b782107bd18fb308d786ed706
JHay0112 <jordanhay0112@gmail.com>
2020-01-07 10:54:02 +1300

    Blog

---
+++ blog.php
@@ -0,0 +1,24 @@
+<?php 
+
+    /* 
+        blog.php
+        Author: Jordan Hay
+    */
+
+    $title = "Blog";
+
+    include("res/head.php");
+
+?>
+
+<main class="row">
+    <aside class="col-5" id="side-img" style="background-image: url('img/blog.jpg');"></aside>
+    <section id="subpage-content" class="col-7">
+        <h1 class="desktop-only">Coming Soon</h1>
+        <h1 class="mobile-only">Coming Soon</h1>
+        <hr />
+
+    </section>
+</main>
+
+<?php include("res/foot.php") ?>
