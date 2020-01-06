b50db4bd7cb8f21b31f62a0766058e65f270dadc
JHay0112 <jordanhay0112@gmail.com>
2020-01-07 11:03:18 +1300

    Vacancies

---
+++ vacancies.php
@@ -0,0 +1,31 @@
+<?php 
+
+    /* 
+        vacancies.php
+        Author: Jordan Hay
+    */
+
+    $title = "Vacancies";
+
+    include("res/head.php");
+
+?>
+
+<main class="row">
+    <aside class="col-5" id="side-img" style="background-image: url('img/vacancies.jpg');"></aside>
+    <div class="col-5"></div>
+    <section id="subpage-content" class="col-7">
+        <h1 class="desktop-only">Vacancies</h1>
+        <h1 class="mobile-only">Vacancies</h1>
+        <hr />
+
+        <p>In the near future, tOpTIMAL coaching Ltd will have positions on offer including: </p>
+        <ul>
+            <li>Business  - Accounting and Admin </li>
+            <li>Socials Optimiser</li>
+        </ul>
+        <p>2020 will also offer intern and mentorship opportunities in optimal coaching and ketOPTIMAL training. Please feel free to get in touch if interested in a future optimal coaching career.</p>
+    </section>
+</main>
+
+<?php include("res/foot.php") ?>
